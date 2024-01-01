4315ddd5587edf9d8b7b15da091fcf08ce228705
gugacavalieri <gugacavalieri@gmail.com>
2024-01-01 15:17:29 -0700

    AVeryBigSum: Add HackerRank Test Case #2

--- algorithms/warmup/a-very-big-sum/main_test.go
+++ algorithms/warmup/a-very-big-sum/main_test.go
@@ -93,3 +93,18 @@ func TestAVeryBigSumTwoNegativeNumbers(t *testing.T) {
 		t.Errorf(`Result = %v, Expected Result = %v`, result, expectedResult)
 	}
 }
+
+/**
+ * Test Case: HackerRank Test Case #2
+ * Input: [1001458909, 1004570889, 1007019111, 1003302837, 1002514638, 1006431461, 1002575010, 1007514041, 1007548981, 1004402249]
+ * Expects Return: 10047338126
+ */
+func TestAVeryBigSumHackerRankTestTwo(t *testing.T) {
+	input := []int64{1001458909, 1004570889, 1007019111, 1003302837, 1002514638, 1006431461, 1002575010, 1007514041, 1007548981, 1004402249}
+	expectedResult := int64(10047338126)
+	result := aVeryBigSum(input)
+
+	if result != expectedResult {
+		t.Errorf(`Result = %v, Expected Result = %v`, result, expectedResult)
+	}
+}
